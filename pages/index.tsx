75eeffbefd027cbe8fc382fbeac913f70c849b6d
blangka <chaehong73@gmai.com>
2022-03-11 15:52:12 +0900

    todo 기본 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from "next";
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
 
-const app: NextPage = () => {
-  return <div>hello</div>;
+const todos: TodoType[] = [
+    { id: 1, text: "마트에 가기", color: "red", checked: false },
+    { id: 2, text: "집에 가기", color: "orange", checked: false },
+    { id: 3, text: "학교에 가기", color: "yellow", checked: false },
+    { id: 4, text: "교실에 가기", color: "blue", checked: false },
+];
+
+const index: NextPage = () => {
+  return (
+    <TodoList todos={todos} />
+  );
 };
 
-export default app;
+export default index;
